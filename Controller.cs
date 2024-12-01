cfdcd1598a67a48287d42008f5720618f9cb929e
AbdelrahmanMohamed-Fathy <abdelrahman.fathy04@outlook.com>
2024-12-01 13:25:55 +0200

    Add project files.

---
+++ Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data;
+
+namespace Movie_Booking_System
+{
+    public class Controller
+    {
+        DBManager dbMan;
+        public Controller()
+        {
+            dbMan = new DBManager();
+        }
+
+        //SQL Queries to be added here, just like back in ~nam~ labs
+        public void TerminateConnection()
+        {
+            dbMan.CloseConnection();
+        }
+    }
+}
