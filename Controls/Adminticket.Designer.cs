659e6d0bc8c936bb58f3f453f768db41e181236f
a7medsoltan2004@gmail.com <a7medsoltan2004@gmail.com>
2024-12-27 11:42:05 +0200

    removed unneeded icons and added some changes to admin dashboard

---
+++ Controls/Adminticket.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Movie_Booking_System.Controls
+{
+    partial class Adminticket
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.combobxTicketId = new System.Windows.Forms.ComboBox();
+            this.lblTicketHeader = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 374);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(512, 176);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // combobxTicketId
+            // 
+            this.combobxTicketId.BackColor = System.Drawing.Color.MintCream;
+            this.combobxTicketId.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.combobxTicketId.ForeColor = System.Drawing.Color.Silver;
+            this.combobxTicketId.FormattingEnabled = true;
+            this.combobxTicketId.Location = new System.Drawing.Point(25, 81);
+            this.combobxTicketId.Name = "combobxTicketId";
+            this.combobxTicketId.Size = new System.Drawing.Size(121, 26);
+            this.combobxTicketId.TabIndex = 1;
+            this.combobxTicketId.Text = "Ticket ID.";
+            this.combobxTicketId.SelectedIndexChanged += new System.EventHandler(this.combobxTicketId_SelectedIndexChanged);
+            // 
+            // lblTicketHeader
+            // 
+            this.lblTicketHeader.AutoSize = true;
+            this.lblTicketHeader.Location = new System.Drawing.Point(22, 166);
+            this.lblTicketHeader.Name = "lblTicketHeader";
+            this.lblTicketHeader.Size = new System.Drawing.Size(56, 16);
+            this.lblTicketHeader.TabIndex = 2;
+            this.lblTicketHeader.Text = "Header.";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 215);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Adminticket
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblTicketHeader);
+            this.Controls.Add(this.combobxTicketId);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Adminticket";
+            this.Size = new System.Drawing.Size(518, 553);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox combobxTicketId;
+        private System.Windows.Forms.Label lblTicketHeader;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
--- Screens/HelpTickets/employeeReviewHelpTicket.Designer.cs
+++ Screens/HelpTickets/employeeReviewHelpTicket.Designer.cs
@@ -47,18 +47,21 @@
             this.dataGridView1.AllowUserToResizeRows = false;
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(26, 45);
+            this.dataGridView1.Location = new System.Drawing.Point(35, 55);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(436, 185);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(581, 228);
             this.dataGridView1.TabIndex = 0;
             // 
             // cmbboxTicketID
             // 
             this.cmbboxTicketID.FormattingEnabled = true;
-            this.cmbboxTicketID.Location = new System.Drawing.Point(521, 101);
+            this.cmbboxTicketID.Location = new System.Drawing.Point(695, 124);
+            this.cmbboxTicketID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmbboxTicketID.Name = "cmbboxTicketID";
-            this.cmbboxTicketID.Size = new System.Drawing.Size(121, 21);
+            this.cmbboxTicketID.Size = new System.Drawing.Size(160, 24);
             this.cmbboxTicketID.Sorted = true;
             this.cmbboxTicketID.TabIndex = 1;
             this.cmbboxTicketID.SelectedIndexChanged += new System.EventHandler(this.cmbboxTicketID_SelectedIndexChanged);
@@ -66,12 +69,13 @@
             // txtbxContent
             // 
             this.txtbxContent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtbxContent.Location = new System.Drawing.Point(11, 274);
+            this.txtbxContent.Location = new System.Drawing.Point(15, 337);
+            this.txtbxContent.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtbxContent.Multiline = true;
             this.txtbxContent.Name = "txtbxContent";
             this.txtbxContent.ReadOnly = true;
             this.txtbxContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtbxContent.Size = new System.Drawing.Size(571, 187);
+            this.txtbxContent.Size = new System.Drawing.Size(760, 229);
             this.txtbxContent.TabIndex = 2;
             this.txtbxContent.Visible = false;
             // 
@@ -79,9 +83,10 @@
             // 
             this.lblHeader.AutoSize = true;
             this.lblHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblHeader.Location = new System.Drawing.Point(4, 233);
+            this.lblHeader.Location = new System.Drawing.Point(5, 287);
+            this.lblHeader.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblHeader.Name = "lblHeader";
-            this.lblHeader.Size = new System.Drawing.Size(127, 37);
+            this.lblHeader.Size = new System.Drawing.Size(156, 46);
             this.lblHeader.TabIndex = 3;
             this.lblHeader.Text = "Header";
             this.lblHeader.Visible = false;
@@ -93,10 +98,10 @@
             this.btnMark.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnMark.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnMark.ForeColor = System.Drawing.Color.White;
-            this.btnMark.Location = new System.Drawing.Point(649, 101);
-            this.btnMark.Margin = new System.Windows.Forms.Padding(4);
+            this.btnMark.Location = new System.Drawing.Point(865, 124);
+            this.btnMark.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.btnMark.Name = "btnMark";
-            this.btnMark.Size = new System.Drawing.Size(121, 62);
+            this.btnMark.Size = new System.Drawing.Size(161, 76);
             this.btnMark.TabIndex = 6;
             this.btnMark.Text = "Mark Seen";
             this.btnMark.UseVisualStyleBackColor = false;
@@ -107,9 +112,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(474, 82);
+            this.label2.Location = new System.Drawing.Point(632, 101);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(63, 16);
+            this.label2.Size = new System.Drawing.Size(81, 20);
             this.label2.TabIndex = 7;
             this.label2.Text = "Ticket ID:";
             // 
@@ -117,9 +123,10 @@
             // 
             this.lblName.AutoSize = true;
             this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblName.Location = new System.Drawing.Point(473, 464);
+            this.lblName.Location = new System.Drawing.Point(631, 571);
+            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(61, 24);
+            this.lblName.Size = new System.Drawing.Size(78, 29);
             this.lblName.TabIndex = 8;
             this.lblName.Text = "Name";
             this.lblName.Visible = false;
@@ -128,18 +135,19 @@
             // 
             this.lblFrom.AutoSize = true;
             this.lblFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFrom.Location = new System.Drawing.Point(418, 464);
+            this.lblFrom.Location = new System.Drawing.Point(557, 571);
+            this.lblFrom.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblFrom.Name = "lblFrom";
-            this.lblFrom.Size = new System.Drawing.Size(60, 24);
+            this.lblFrom.Size = new System.Drawing.Size(76, 29);
             this.lblFrom.TabIndex = 11;
             this.lblFrom.Text = "From:";
             this.lblFrom.Visible = false;
             // 
             // employeeReviewHelpTicket
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(783, 514);
+            this.ClientSize = new System.Drawing.Size(1044, 633);
             this.Controls.Add(this.lblFrom);
             this.Controls.Add(this.lblName);
             this.Controls.Add(this.label2);
@@ -148,6 +156,7 @@
             this.Controls.Add(this.txtbxContent);
             this.Controls.Add(this.cmbboxTicketID);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "employeeReviewHelpTicket";
             this.Text = "employeeReviewHelpTicket";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
